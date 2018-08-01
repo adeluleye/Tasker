2edbf370784f0218b9a0780f35e7c53fea44a02e
adeluleye <adelu4real@gmail.com>
2018-08-01 22:24:24 +0100

    Restructure the App to MVC design pattern

--- Tasker/Controllers/TodoListViewController.swift
+++ Tasker/Controllers/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["Learn swift", "Learn Java", "Learn Kotlin"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
 
@@ -18,7 +18,19 @@ class TodoListViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+        let newItem = Item()
+        newItem.title = "Learn Kotlin"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Learn Java"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Learn Swift"
+        itemArray.append(newItem3)
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
             itemArray = items
         }
         
@@ -30,7 +42,21 @@ class TodoListViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        //Using Tenary operator, we can achieve the if statement as:
+        //value = condition ? valueIfTrue : valueIfFalse
+        
+        //cell.accessoryType = item.done == true ? .checkmark : .none
+        //OR
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+//        if item.done == true {
+//            cell.accessoryType = .checkmark
+//        } else {
+//            cell.accessoryType = .none
+//        }
         
         //This display text in secondary label if one exist
         //cell.detailTextLabel?.text = itemArray[indexPath.row]
@@ -45,15 +71,23 @@ class TodoListViewController: UITableViewController {
     // MARK: Tableview Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(itemArray[indexPath.row])
+        //print(itemArray[indexPath.row])
         
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+//        if itemArray[indexPath.row].done == false {
+//            itemArray[indexPath.row].done = true
+//        } else {
+//            itemArray[indexPath.row].done = false
+//        }
         
+//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        } else {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
+        
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -69,7 +103,9 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Task", style: .default) { (action) in
             // what will happen when the user clicks the Add Task button on the UIAlert
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
---
+++ Tasker/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Tasker
+//
+//  Created by ADELU ABIDEEN ADELEYE on 8/1/18.
+//  Copyright © 2018 Spantom Technologies Ltd. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+}
